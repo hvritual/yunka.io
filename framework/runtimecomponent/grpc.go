23d46fab019549f08a97f9cdcd77eb6ec0a65e8a
hvritual <460657446@qq.com>
2026-08-30 09:40:48 +0800

    feat(c10.3): adapt explicit gRPC server into App lifecycle

---
+++ framework/runtimecomponent/grpc.go
@@ -0,0 +1,136 @@
+package runtimecomponent
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net"
+	"strings"
+	"sync"
+
+	grpcgo "google.golang.org/grpc"
+	"yunka.io/framework/core"
+)
+
+type GRPCOptions struct {
+	Name     string
+	Server   *grpcgo.Server
+	Listener net.Listener
+}
+
+func GRPC(options GRPCOptions) (core.RuntimeComponent, error) {
+	name := strings.TrimSpace(options.Name)
+	if name == "" {
+		return core.RuntimeComponent{}, errors.New("runtimecomponent: gRPC component name is required")
+	}
+	if options.Server == nil {
+		return core.RuntimeComponent{}, errors.New("runtimecomponent: gRPC server is required")
+	}
+	if options.Listener == nil {
+		return core.RuntimeComponent{}, errors.New("runtimecomponent: gRPC listener is required")
+	}
+	runtime := &grpcRuntime{name: name, server: options.Server, listener: options.Listener}
+	return core.RuntimeComponent{Name: name, StartFunc: runtime.start, HealthFunc: runtime.health, ShutdownFunc: runtime.shutdown}, nil
+}
+
+type grpcRuntime struct {
+	name     string
+	server   *grpcgo.Server
+	listener net.Listener
+
+	mu       sync.RWMutex
+	started  bool
+	stopping bool
+	exited   bool
+	serveErr error
+	done     chan struct{}
+}
+
+func (runtime *grpcRuntime) start(context.Context) error {
+	runtime.mu.Lock()
+	if runtime.started {
+		runtime.mu.Unlock()
+		return nil
+	}
+	if runtime.stopping {
+		runtime.mu.Unlock()
+		return fmt.Errorf("runtimecomponent: gRPC %s is stopping", runtime.name)
+	}
+	runtime.started = true
+	runtime.done = make(chan struct{})
+	runtime.mu.Unlock()
+
+	go func() {
+		err := runtime.server.Serve(runtime.listener)
+		if errors.Is(err, grpcgo.ErrServerStopped) || errors.Is(err, net.ErrClosed) {
+			err = nil
+		}
+		runtime.mu.Lock()
+		runtime.exited = true
+		runtime.serveErr = err
+		close(runtime.done)
+		runtime.mu.Unlock()
+	}()
+	return nil
+}
+
+func (runtime *grpcRuntime) health(context.Context) error {
+	runtime.mu.RLock()
+	defer runtime.mu.RUnlock()
+	if !runtime.started {
+		return fmt.Errorf("runtimecomponent: gRPC %s has not started", runtime.name)
+	}
+	if runtime.exited && !runtime.stopping {
+		if runtime.serveErr != nil {
+			return fmt.Errorf("runtimecomponent: gRPC %s exited: %w", runtime.name, runtime.serveErr)
+		}
+		return fmt.Errorf("runtimecomponent: gRPC %s exited unexpectedly", runtime.name)
+	}
+	return nil
+}
+
+func (runtime *grpcRuntime) shutdown(ctx context.Context) error {
+	if ctx == nil {
+		ctx = context.Background()
+	}
+	runtime.mu.Lock()
+	if runtime.stopping {
+		done := runtime.done
+		runtime.mu.Unlock()
+		return waitRuntimeDone(ctx, done)
+	}
+	runtime.stopping = true
+	started := runtime.started
+	done := runtime.done
+	runtime.mu.Unlock()
+
+	if !started {
+		runtime.server.Stop()
+		if err := runtime.listener.Close(); err != nil && !errors.Is(err, net.ErrClosed) {
+			return fmt.Errorf("runtimecomponent: close gRPC %s listener: %w", runtime.name, err)
+		}
+		return nil
+	}
+
+	gracefulDone := make(chan struct{})
+	go func() {
+		runtime.server.GracefulStop()
+		close(gracefulDone)
+	}()
+
+	var gracefulErr error
+	select {
+	case <-gracefulDone:
+	case <-ctx.Done():
+		runtime.server.Stop()
+		<-gracefulDone
+		gracefulErr = ctx.Err()
+	}
+	if err := runtime.listener.Close(); err != nil && !errors.Is(err, net.ErrClosed) {
+		gracefulErr = errors.Join(gracefulErr, fmt.Errorf("runtimecomponent: close gRPC %s listener: %w", runtime.name, err))
+	}
+	if err := waitRuntimeDone(context.Background(), done); err != nil {
+		gracefulErr = errors.Join(gracefulErr, err)
+	}
+	return gracefulErr
+}
