00365e54525e26a78889481a15c3a440495356d9
hvritual <460657446@qq.com>
2026-09-01 07:47:15 +0800

    fix(devruntime): own Unix process groups

--- pkg/devruntime/process_signal_unix.go
+++ pkg/devruntime/process_signal_unix.go
@@ -3,13 +3,46 @@
 package devruntime
 
 import (
+	"errors"
 	"os"
+	"os/exec"
 	"syscall"
 )
 
+func prepareProcess(command *exec.Cmd) {
+	if command == nil {
+		return
+	}
+	if command.SysProcAttr == nil {
+		command.SysProcAttr = &syscall.SysProcAttr{}
+	}
+	command.SysProcAttr.Setpgid = true
+}
+
 func signalProcess(process *os.Process) error {
+	return signalProcessGroup(process, syscall.SIGTERM)
+}
+
+func killProcess(process *os.Process) error {
+	return signalProcessGroup(process, syscall.SIGKILL)
+}
+
+func signalProcessGroup(process *os.Process, signal syscall.Signal) error {
 	if process == nil {
 		return nil
 	}
-	return process.Signal(syscall.SIGTERM)
+	pgid, err := syscall.Getpgid(process.Pid)
+	if err != nil {
+		if errors.Is(err, syscall.ESRCH) {
+			return os.ErrProcessDone
+		}
+		return err
+	}
+	if err := syscall.Kill(-pgid, signal); err != nil {
+		if errors.Is(err, syscall.ESRCH) {
+			return os.ErrProcessDone
+		}
+		return err
+	}
+	return nil
 }
