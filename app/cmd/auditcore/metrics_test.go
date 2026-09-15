322c2c8d4e54212137c9e3684e106cd94f2cb6eb
hvritual <460657446@qq.com>
2026-09-15 11:40:40 +0800

    test(audit): prove deterministic structural metrics

---
+++ app/cmd/auditcore/metrics_test.go
@@ -0,0 +1,41 @@
+package auditcore
+
+import (
+	"go/parser"
+	"go/token"
+	"testing"
+)
+
+func TestMeasureSourceCountsDeclaredStructuralMetrics(t *testing.T) {
+	contents := []byte(`package sample
+
+var A = 1
+
+type B struct{}
+
+func Work(v int) {
+	if v > 0 {
+		for i := 0; i < v; i++ {
+			switch i {
+			case 0:
+			case 1:
+			}
+		}
+	}
+}
+`)
+	file, err := parser.ParseFile(token.NewFileSet(), "sample.go", contents, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	metrics := measureSource(file, contents)
+	if metrics.Lines != 17 {
+		t.Fatalf("lines=%d want=17", metrics.Lines)
+	}
+	if metrics.TopLevelDeclarations != 3 {
+		t.Fatalf("topLevelDeclarations=%d want=3", metrics.TopLevelDeclarations)
+	}
+	if metrics.BranchPoints != 5 {
+		t.Fatalf("branchPoints=%d want=5", metrics.BranchPoints)
+	}
+}
